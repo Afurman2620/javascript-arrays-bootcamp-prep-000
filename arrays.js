e98be30f544d1260b905cb3178eea2d03b706c95
Artem  F <artemfm@gmail.com>
2021-02-01 01:32:11 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -31,7 +31,7 @@ function accessElementInArray(array, index){
 return (array [index])
 }
 
-function destructivelyAddElementToBeginningOfArray (array){
+function destructivelyRemoveElementFromBeginningOfArray(array){
 array.shift()
 return array
 }
