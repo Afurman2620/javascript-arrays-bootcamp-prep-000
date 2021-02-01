068a508b495acb9f93e55a8a63c33081f5231fed
Artem  F <artemfm@gmail.com>
2021-02-01 01:38:41 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -40,4 +40,6 @@ function removeElementFromBeginningOfArray(array){
  return array.slice(1)
 }
 
-function destructivelyRemoveElementFromEndOfArray(array)
+function destructivelyRemoveElementFromEndOfArray(array){
+  
+}
